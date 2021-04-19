880f4232cf7fd6fcda3aef61e7eaf3db1bd960d7
Manuel Clifton <manuclifton@gmail.com>
2021-04-19 12:02:39 -0300

    fix entity document

--- LOAS/Entidades/Modules/Module.cs
+++ LOAS/Entidades/Modules/Module.cs
@@ -9,14 +9,21 @@ namespace Entidades
     public abstract class Module
     {
         List<Employe> listEmployes;
-        public Module()
+        string name;
+        public Module(string name)
         {
             listEmployes = new List<Employe>();
+            this.Name = name;
         }
         public Employe this[int index]
         {
             get { return this.listEmployes[index]; }
             set { this.listEmployes[index] = value; }
+        }
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
         }
 
 
@@ -33,13 +40,25 @@ namespace Entidades
             }
             return false;
         }
+        public static bool operator -(Module mod, Employe emp)
+        {
 
+            if (!(mod is null) && !(emp is null))
+            {
+                if (mod.listEmployes.Contains(emp))
+                {
+                    mod.listEmployes.Remove(emp);
+                    return true;
+                }
+            }
+            return false;
+        }
         public static bool operator ==(Module mod1, Module mod2)
         {
 
             if (!(mod1 is null) && !(mod2 is null))
             {
-                if (mod1.colorSala == mod2.colorSala ) // chequear cuando es igual un modulo a otro
+                if (mod1.Name == mod2.Name && mod1.listEmployes == mod2.listEmployes ) // chequear cuando es igual un modulo a otro
                 {
                     return true;
                 }
--- LOAS/Entidades/TypeDocuments/Document.cs
+++ LOAS/Entidades/TypeDocuments/Document.cs
@@ -8,9 +8,36 @@ namespace Entidades
 {
     public abstract class Document
     {
-        public Document() { }
+        protected static DateTime fechaCreacion = new DateTime();
+        protected static string entidad = "L.O.A.S";
+        protected Employe emisor;
+        protected Employe receptor;
+        //agregar arrray de estados [creado-ejecucion-listo-termiando-cancelado]
+        protected string[] estados = new string[] { "creado", "ejecucion", "listo", "termiando", "cancelado" };
+        protected string estadoActual;
+        public Document(){ }
 
-
+        protected Document(Employe emisor, Employe receptor)
+        {
+            this.Emisor = emisor;
+            this.Reseptor = receptor;
+            this.EstadoActual = "creado";
+        }
+        public Employe Emisor
+        {
+            get { return this.emisor; }
+            set { this.emisor = value; }
+        }
+        public Employe Reseptor
+        {
+            get { return this.receptor; }
+            set { this.receptor = value; }
+        }
+        public string EstadoActual
+        {
+            get { return this.estadoActual; }
+            set { this.estadoActual = value; }
+        }
 
 
     }//
